8482b602e9b875289726bab1ee0e840496545cda
futao@ghostcloud.cn <futao@ghostcloud.cn>
2019-03-04 11:05:11 +0800

    ok v1

--- collector/skywalking-apm-collector/bin/oapService.sh
+++ collector/skywalking-apm-collector/bin/oapService.sh
@@ -16,22 +16,18 @@
 
 #!/usr/bin/env sh
 
-sed -i "s/{{SW_AGENT_COLLECTOR_BACKEND_SERVICES}}/${SW_AGENT_COLLECTOR_BACKEND_SERVICES}/g" agent/config/agent.config
+sed -i "s/{{SW_CORE_REST_HOST}}/${SW_CORE_REST_HOST}/g" config/application.yml
+sed -i "s/{{SW_CORE_REST_PORT}}/${SW_CORE_REST_PORT}/g" config/application.yml
+sed -i "s/{{SW_CORE_GRPC_HOST}}/${SW_CORE_GRPC_HOST}/g" config/application.yml
+sed -i "s/{{SW_CORE_GRPC_PORT}}/${SW_CORE_GRPC_PORT}/g" config/application.yml
 
-cat agent/config/agent.config
-
- sed -i "s/{{SW_CORE_REST_HOST}}/${SW_CORE_REST_HOST}/g" config/application.yml
- sed -i "s/{{SW_CORE_REST_PORT}}/${SW_CORE_REST_PORT}/g" config/application.yml
- sed -i "s/{{SW_CORE_GRPC_HOST}}/${SW_CORE_GRPC_HOST}/g" config/application.yml
- sed -i "s/{{SW_CORE_GRPC_PORT}}/${SW_CORE_GRPC_PORT}/g" config/application.yml
-
- sed -i "s/{{SW_STORAGE_ES_CLUSTER_NODES}}/${SW_STORAGE_ES_CLUSTER_NODES}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_INDEX_SHARDS_NUMBER}}/${SW_STORAGE_ES_INDEX_SHARDS_NUMBER}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_INDEX_REPLICAS_NUMBER}}/${SW_STORAGE_ES_INDEX_REPLICAS_NUMBER}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_BULK_ACTIONS}}/${SW_STORAGE_ES_BULK_ACTIONS}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_BULK_SIZE}}/${SW_STORAGE_ES_BULK_SIZE}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_FLUSH_INTERVAL}}/${SW_STORAGE_ES_FLUSH_INTERVAL}/g" config/application.yml
- sed -i "s/{{SW_STORAGE_ES_CONCURRENT_REQUESTS}}/${SW_STORAGE_ES_CONCURRENT_REQUESTS}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_CLUSTER_NODES}}/${SW_STORAGE_ES_CLUSTER_NODES}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_INDEX_SHARDS_NUMBER}}/${SW_STORAGE_ES_INDEX_SHARDS_NUMBER}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_INDEX_REPLICAS_NUMBER}}/${SW_STORAGE_ES_INDEX_REPLICAS_NUMBER}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_BULK_ACTIONS}}/${SW_STORAGE_ES_BULK_ACTIONS}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_BULK_SIZE}}/${SW_STORAGE_ES_BULK_SIZE}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_FLUSH_INTERVAL}}/${SW_STORAGE_ES_FLUSH_INTERVAL}/g" config/application.yml
+sed -i "s/{{SW_STORAGE_ES_CONCURRENT_REQUESTS}}/${SW_STORAGE_ES_CONCURRENT_REQUESTS}/g" config/application.yml
 
 cat config/application.yml
 
